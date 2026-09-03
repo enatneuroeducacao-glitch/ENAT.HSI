f708fb030bbfae48dae0d7e739bb1af47f598954
enatneuroeducacao-glitch <enat.neuroeducacao@gmail.com>
2026-09-03 00:33:05 -0300

    feat: upgrade central certificate issuer

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { Certificados } from "./pages/Certificados";
 import { DashboardAluno } from "./pages/DashboardAluno";
 import { RelatorioTurmas } from "./pages/RelatorioTurmas";
 import { IndicacoesInstrutor } from "./pages/IndicacoesInstrutor";
-import { EmissorCertificadosCloud } from "./pages/EmissorCertificadosCloud";
+import { EmissorCertificadosAdmin } from "./pages/EmissorCertificadosAdmin";
 import { ValidarCertificadoPublico } from "./pages/ValidarCertificadoPublico";
 
 export default function CentralENATHSI() {
@@ -41,7 +41,7 @@ export default function CentralENATHSI() {
         <Route path="/testes" element={<TestesAvancados />} />
         <Route path="/testes/:testId" element={<TesteAprofundado />} />
         <Route path="/certificados" element={<Certificados />} />
-        <Route path="/emissor" element={<EmissorCertificadosCloud />} />
+        <Route path="/emissor" element={<EmissorCertificadosAdmin />} />
         <Route path="/validar/:code" element={<ValidarCertificadoPublico />} />
         <Route path="/dashboard" element={<DashboardAluno />} />
         <Route path="/relatorio-turmas" element={<RelatorioTurmas />} />
