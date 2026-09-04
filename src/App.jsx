0f4e66c71111703693eda6d1768d8988d05f9b0d
enatneuroeducacao-glitch <enat.neuroeducacao@gmail.com>
2026-09-04 16:12:27 -0300

    Adicionar rota administrativa do Banco de Cursos ENAT

--- src/App.jsx
+++ src/App.jsx
@@ -20,34 +20,10 @@ import { RelatorioTurmas } from "./pages/RelatorioTurmas";
 import { IndicacoesInstrutor } from "./pages/IndicacoesInstrutor";
 import { EmissorCertificadosFinal } from "./pages/EmissorCertificadosFinal";
 import { ValidarCertificadoPublico } from "./pages/ValidarCertificadoPublico";
+import { BancoCursosENAT } from "./pages/BancoCursosENAT";
 
 export default function CentralENATHSI() {
-  return (
-    <BrowserRouter>
-      <MenuENAT />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<SobreENAT />} />
-        <Route path="/produtos" element={<ProdutosENAT />} />
-        <Route path="/produtos/:produto" element={<ProdutoDetail />} />
-        <Route path="/certificacao" element={<CertificacaoENAT />} />
-        <Route path="/governanca" element={<GovernancaENAT />} />
-        <Route path="/contato" element={<ContatoENAT />} />
-        <Route path="/resultados" element={<ResultadosENAT />} />
-        <Route path="/cadastro/instrutor" element={<CadastroInstrutor />} />
-        <Route path="/cadastro/aluno" element={<CadastroAluno />} />
-        <Route path="/cursos" element={<CursosENAT />} />
-        <Route path="/cursos/:courseId" element={<CursoDetalhado />} />
-        <Route path="/testes" element={<TestesAvancados />} />
-        <Route path="/testes/:testId" element={<TesteAprofundado />} />
-        <Route path="/certificados" element={<Certificados />} />
-        <Route path="/emissor" element={<EmissorCertificadosFinal />} />
-        <Route path="/validar/:code" element={<ValidarCertificadoPublico />} />
-        <Route path="/dashboard" element={<DashboardAluno />} />
-        <Route path="/relatorio-turmas" element={<RelatorioTurmas />} />
-        <Route path="/indicacoes" element={<IndicacoesInstrutor />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <BrowserRouter><MenuENAT/><Routes>
+    <Route path="/" element={<Home/>}/><Route path="/sobre" element={<SobreENAT/>}/><Route path="/produtos" element={<ProdutosENAT/>}/><Route path="/produtos/:produto" element={<ProdutoDetail/>}/><Route path="/certificacao" element={<CertificacaoENAT/>}/><Route path="/governanca" element={<GovernancaENAT/>}/><Route path="/contato" element={<ContatoENAT/>}/><Route path="/resultados" element={<ResultadosENAT/>}/><Route path="/cadastro/instrutor" element={<CadastroInstrutor/>}/><Route path="/cadastro/aluno" element={<CadastroAluno/>}/><Route path="/cursos/admin" element={<BancoCursosENAT/>}/><Route path="/cursos" element={<CursosENAT/>}/><Route path="/cursos/:courseId" element={<CursoDetalhado/>}/><Route path="/testes" element={<TestesAvancados/>}/><Route path="/testes/:testId" element={<TesteAprofundado/>}/><Route path="/certificados" element={<Certificados/>}/><Route path="/emissor" element={<EmissorCertificadosFinal/>}/><Route path="/validar/:code" element={<ValidarCertificadoPublico/>}/><Route path="/dashboard" element={<DashboardAluno/>}/><Route path="/relatorio-turmas" element={<RelatorioTurmas/>}/><Route path="/indicacoes" element={<IndicacoesInstrutor/>}/><Route path="*" element={<Home/>}/>
+  </Routes></BrowserRouter>;
 }
